1a5a5ca367ee6c2030d4fb3a787a487618ed386b
FerrianGardson <FerrianGardson@gmail.com>
2025-06-17 14:57:08 +0500

    Добавил обычный HTML+JS

---
+++ html/TISO_files/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/340ae8ae28462567.css","static/chunks/pages/index-06ee314f688ae12d.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
